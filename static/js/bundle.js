5ccfbd87c4ad928fba13e387c1980c2c9a4dde51
erlams <elmaddinakhmedzadeh@gmail.com>
2025-09-22 11:38:01 +0400

    bundle

---
+++ static/js/bundle.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 5, // ✅ indi ekranda 5 slayd görsənir
+  spaceBetween: 20, // ✅ slaydlar arasında məsafə
+  loop: true,
+  coverflowEffect: {
+    rotate: 30,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 3 },
+    1200: { slidesPerView: 5 }, // ✅ böyük ekranda 5
+  },
+});
